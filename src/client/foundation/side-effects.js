69c3495e48b11eed1243f01d22334903994e74f1
sor4chi <sorakawa0218@gmail.com>
2023-02-28 23:03:38 +0900

    fontawesome-jsをやめる

--- src/client/foundation/side-effects.js
+++ src/client/foundation/side-effects.js
@@ -2,6 +2,3 @@ import "regenerator-runtime/runtime";
 import "es5-shim";
 import "es6-shim";
 import "es7-shim";
-import "@fortawesome/fontawesome-free/js/fontawesome";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/regular";
