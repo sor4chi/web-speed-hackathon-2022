41020d931d12cb57115f1409bb7cb0691ad6e6ec
sor4chi <sorakawa0218@gmail.com>
2023-03-01 22:57:56 +0900

    odds_itemのraceIdへindexを付与

--- src/server/typeorm/initialize.js
+++ src/server/typeorm/initialize.js
@@ -1,7 +1,19 @@
 import fs from "fs/promises";
 
+import sqlite3 from "sqlite3";
+
 import { DATABASE_PATH, INITIAL_DATABASE_PATH } from "../paths.js";
 
 export async function initialize() {
   await fs.copyFile(INITIAL_DATABASE_PATH, DATABASE_PATH);
+
+  const db = new sqlite3.Database(DATABASE_PATH);
+  const run = (sql) =>
+    new Promise((resolve, reject) => {
+      const res = db.run(sql);
+      res.on("error", reject);
+      res.on("finish", resolve);
+    });
+
+  await run("CREATE INDEX idx_race_id ON odds_item (raceId);");
 }
